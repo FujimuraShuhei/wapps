fd92c53ed35c50071a3f1133b081a0794d917619
shuhei_fujimura <shushu2525.1694@gmail.com>
2024-12-31 10:50:30 +0900

    環境構築完了

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import styles from '../pages/style.module.scss';
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div className={styles['input']}>あああああ</div>
     </>
   );
 }
